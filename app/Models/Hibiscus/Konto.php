1e5d6ad07f9e0d7768f23413afed7c5c02ed9af3
Markus Koehler <info@koehler.pro>
2018-11-25 17:31:32 +0100

    the 2 essential hibiscus models Konto & Umsatz (account & transactions), extending the BaseModel to be cachable

---
+++ app/Models/Hibiscus/Konto.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Hibiscus;
+
+use App\Models\BaseModel;
+
+class Konto extends BaseModel
+{
+    protected $connection = 'hibiscus';
+
+    protected $table = 'konto';
+}
